f371a531f51b2b4bab3d50fd7362aa393e4de502
kimson <xpfjrnseks1@naver.com>
2022-11-01 02:11:50 +0900

    업데이트 로직 수정

---
+++ src/scripts/Component.js
@@ -0,0 +1,73 @@
+import { $ } from "./tools";
+
+class Component {
+  #target;
+  #htmlStrings;
+
+  constructor() {}
+
+  set target(el) {
+    this.#target = $(el);
+  }
+
+  get target() {
+    return this.#target;
+  }
+
+  format(form, time) {
+    return form.replace(/YYYY|MM|DD|HH|mm|ss|SSS|AP/g, ($1) => {
+      const year = time.getFullYear();
+      const month = time.getMonth();
+      const day = time.getMinutes();
+      const hours = time.getHours();
+      const minutes = time.getMinutes();
+      const seconds = time.getSeconds();
+      const milliseconds = time.getMilliseconds();
+      const isOver = hours > 12;
+      const padding = (val) => val.toString().padStart(2, 0);
+
+      switch ($1) {
+        case "YYYY":
+          return padding(year);
+        case "MM":
+          return padding(month);
+        case "DD":
+          return padding(day);
+        case "HH":
+          return padding(hours);
+        case "mm":
+          return padding(minutes);
+        case "ss":
+          return padding(seconds);
+        case "SSS":
+          return padding(milliseconds);
+        case "AP":
+          return isOver ? "PM" : "AM";
+        default:
+          return $1;
+      }
+    });
+  }
+
+  publish(htmlStrings) {
+    this.#htmlStrings = htmlStrings;
+    this.#update();
+  }
+
+  unPublish() {
+    this.#htmlStrings = "";
+  }
+
+  getDate() {
+    return new Date().getTime();
+  }
+  #update() {
+    this.#init();
+    this.#target.innerHTML = this.#htmlStrings;
+  }
+  #init() {
+    this.#target.innerHTML = "";
+  }
+}
+
+export default Component;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,152 @@
-const $ = (el) => document.querySelector(el);
+"use strict";
 
-const todoList = [];
+import Component from "./Component";
+import { $ } from "./tools";
 
-function createTodo() {
-  const todo = $("#insert").value;
-  todoList.push(todo);
-  update();
+const initialMessage = "일정이 없습니다.";
+
+class ArrayComponent extends Component {
+  #stacks = [];
+
+  constructor(el) {
+    super();
+    super.target = el;
+    window.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = e.target;
+      const closest = target.closest(".item");
+      if (!closest) return;
+      const id = closest.dataset.index;
+      const isVisible = !JSON.parse(closest.dataset.value);
+      this.edit(Number(id), "isVisible", isVisible);
+    });
+    window.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = e.target;
+      const closest = target.closest(".del");
+      if (!closest) return;
+      const id = closest.dataset.index;
+      this.delete(Number(id));
+    });
+    window.addEventListener("keydown", (e) => {
+      if (e.key !== "Enter") return;
+
+      this.add({ category: "none", content: $("#insert").value.trim() });
+      $("input#insert").value = "";
+      $("input#insert").focus();
+    });
+    this.render();
+  }
+
+  setWriteButton(el) {
+    $(el).addEventListener("click", () => {
+      this.add({ category: "none", content: $("#insert").value.trim() });
+      $("input#insert").value = "";
+      $("input#insert").focus();
+    });
+  }
+
+  createItem({ id, category, content, isVisible, created_at, updated_at }) {
+    const divEL = document.createElement("div");
+    const numEL = document.createElement("span");
+    const contentsEL = document.createElement("span");
+    const timeWrap = document.createElement("span");
+    const createEL = document.createElement("time");
+    const updateEL = document.createElement("time");
+
+    const delEL = document.createElement("span");
+    delEL.classList.add("del");
+
+    divEL.dataset.index = id;
+    divEL.dataset.value = isVisible;
+    delEL.dataset.index = id;
+
+    divEL.classList.add("item");
+    numEL.classList.add("num");
+    contentsEL.classList.add("content");
+    timeWrap.classList.add("time-wrap");
+    createEL.classList.add("create");
+    updateEL.classList.add("update");
+
+    numEL.innerHTML = id;
+    contentsEL.innerHTML = content;
+    createEL.innerHTML = super.format("YYYY.MM.DD HH:mm:ss", new Date(created_at));
+    updateEL.innerHTML = super.format("YYYY.MM.DD HH:mm:ss", new Date(updated_at));
+    delEL.innerHTML = "❌";
+
+    timeWrap.append(createEL, updateEL);
+    divEL.append(numEL, contentsEL, timeWrap, delEL);
+
+    return divEL;
+  }
+
+  render() {
+    let htmls = initialMessage;
+    if (this.#stacks.length > 0) htmls = "";
+    this.#stacks.forEach((item) => {
+      // if (!item.isVisible) return;
+      htmls += this.createItem(item).outerHTML;
+    });
+    $("#current").innerHTML = this.#stacks.filter(
+      (item) => !item.isVisible
+    ).length;
+    $("#total").innerHTML = this.#stacks.length;
+    super.publish(htmls);
+  }
+
+  add({ category, content }) {
+    this.#stacks.push({
+      id: this.#stacks.length,
+      category,
+      content,
+      isVisible: true,
+      created_at: super.getDate(),
+      updated_at: super.getDate(),
+    });
+    this.render();
+  }
+
+  edit(id, key, value) {
+    this.#stacks[id][key] = value;
+    this.#stacks[id]["updated_at"] = super.getDate();
+    this.render();
+  }
+
+  editAll(object) {
+    delete object["id"];
+    delete object["created_at"];
+    delete object["updated_at"];
+
+    this.#stacks = this.#stacks.map((item, index) => ({
+      ...item,
+      id: index,
+      ...object,
+      updated_at: super.getDate(),
+    }));
+    this.render();
+  }
+
+  delete(id) {
+    this.#stacks.splice(id, 1);
+    this.#stacks = this.#stacks.map((item, index) => ({ ...item, id: index }));
+    this.render();
+  }
+
+  clear() {
+    this.#stacks = [];
+  }
 }
 
-function createTodoItem(content) {
-  clear();
-  const item = document.createElement("div");
-  item.classList.add("item");
-  item.innerHTML = content;
-  return item;
+class IOComponent extends Component {
+  constructor() {
+    super();
+  }
 }
 
-function update() {
-  $("#list").innerHTML = "";
-  todoList.forEach((item) => {
-    $("#list").append(createTodoItem(item));
-  });
-}
+const todoList = new ArrayComponent("#list");
+todoList.setWriteButton("#btn");
 
-function clear() {
-  $("#insert").value = "";
-}
-
-$("#btn").addEventListener("click", createTodo);
+todoList.add({
+  category: "wow",
+  content: "test",
+});
